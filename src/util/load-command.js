80c11276d2f04aff84794a4a119fab90c311bd4c
kubgus <business@gustafik.com>
2025-04-23 11:43:05 +0200

    Make undefined in CommandBuilder more flexible

--- src/util/load-command.js
+++ src/util/load-command.js
@@ -4,14 +4,19 @@ const path = require("path");
 // Returns an object containing the command data, execute function, and cooldown
 module.exports = (file) => {
     const {
-        data = new SlashCommandBuilder()
-            .setDescription("Press ENTER to execute this command."),
+        data = new SlashCommandBuilder(),
         execute,
         cooldown = 0,
         public = false, // Determines whether the command can be used outside of the debug guild
     } = require(path.resolve(`${file}`));
 
-    data.setName(path.basename(file, ".js"));
+    if (!data.name) {
+        data.setName(path.basename(file, ".js"));
+    }
+
+    if (!data.description) {
+        data.setDescription("Press ENTER to execute this command.");
+    }
 
     return { data, execute, cooldown, public };
 }
